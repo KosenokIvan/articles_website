ce6c13a286e38944f8fdb2e74234878399d36b2c
Ivan <kosenoki213@gmail.com>
2021-04-08 12:21:48 +0300

    Добавлена модель User

---
+++ data/users.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+import sqlalchemy
+from sqlalchemy_serializer import SerializerMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = "users"
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    surname = sqlalchemy.Column(sqlalchemy.String)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    nickname = sqlalchemy.Column(sqlalchemy.String, unique=True, index=True)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String)
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.now)
+    avatar = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from data import db_session
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "articles_site"
 
 
+@app.route("/")
+def index():
+    return render_template("index.html", title="main")
+
+
 if __name__ == '__main__':
+    db_session.global_init("db/articles.db")
     app.run()

d7b5b57bab482d3fb1f45e667d88db6ece010b6f
Ivan <kosenoki213@gmail.com>
2021-04-12 20:46:31 +0300

    Добавлен виджет для отображения комментариев

--- data/comments.py
+++ data/comments.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import sqlalchemy
 from sqlalchemy import orm
 from sqlalchemy_serializer import SerializerMixin
@@ -11,5 +12,6 @@ class Comment(SqlAlchemyBase, SerializerMixin):
     article_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("articles.id"))
     text = sqlalchemy.Column(sqlalchemy.String)
     image = sqlalchemy.Column(sqlalchemy.String)
+    create_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.now)
     user = orm.relation("User")
     article = orm.relation("Article")
